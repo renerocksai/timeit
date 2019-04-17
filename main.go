608c72cf349047e660680db5e50ac1aab9cf21b9
Rene Schallner <rene.schallner@nim.org>
2019-04-17 18:16:55 +0200

    better timing

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
 	start := time.Now()
-	if c, err := exec.Command(os.Args[1], os.Args[2:]...).CombinedOutput(); err != nil {
+	if output, err := cmd.CombinedOutput(); err != nil {
 		log.Fatal(err)
 	} else {
 		end := time.Now()
 		elapsed := end.Sub(start)
 		fmt.Printf("Elapsed: %s", elapsed)
-		fmt.Printf("%s\n", c)
+		fmt.Printf("%s\n", output)
 	}
-
 }
